3625c98b1a2e3749368a5c183b5406df2a2edca0
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-10 19:25:32 +0500

    Inject view model

--- app/src/main/java/com/wigroup/dependencyinjection/example2/presentation/ExampleViewModel.kt
+++ app/src/main/java/com/wigroup/dependencyinjection/example2/presentation/ExampleViewModel.kt
@@ -1,13 +1,15 @@
 package com.wigroup.dependencyinjection.example2.presentation
 
+import androidx.lifecycle.ViewModel
 import com.wigroup.dependencyinjection.example2.domain.ExampleUseCase
 import javax.inject.Inject
 
 class ExampleViewModel @Inject constructor(
     private val useCase: ExampleUseCase
-) {
+) : ViewModel() {
 
     fun method() {
         useCase()
+        println(this)
     }
 }
--- app/src/main/java/com/wigroup/dependencyinjection/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/wigroup/dependencyinjection/example2/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.wigroup.dependencyinjection.R
 import com.wigroup.dependencyinjection.example2.ExampleApp
 import javax.inject.Inject
@@ -12,7 +13,15 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var viewModel: ExampleViewModel
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel::class.java]
+    }
+
+    private val viewModel2 by lazy {
+        ViewModelProvider(this, viewModelFactory)[ExampleViewModel2::class.java]
+    }
 
     private val component by lazy {
         (application as ExampleApp).component
@@ -30,5 +39,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         viewModel.method()
+        viewModel2.method()
     }
 }
