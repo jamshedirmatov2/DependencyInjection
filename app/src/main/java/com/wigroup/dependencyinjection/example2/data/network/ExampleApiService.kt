c525b4a0b5b0c9ec6c1719a8cf6509ad530ab08c
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-10 18:09:03 +0500

    Use custom scopes

--- app/src/main/java/com/wigroup/dependencyinjection/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/com/wigroup/dependencyinjection/example2/data/network/ExampleApiService.kt
@@ -3,9 +3,7 @@ package com.wigroup.dependencyinjection.example2.data.network
 import android.content.Context
 import com.wigroup.dependencyinjection.R
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 class ExampleApiService @Inject constructor(
     private val context: Context,
     private val timeMillis: Long
